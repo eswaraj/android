8e7f518764bb5d04b2adf6fd4f51dfeccb29c25b
gvv@fueled.co <gvv@fueled.co>
2013-07-04 15:13:20 +0530

    map added

--- JanSampark/src/com/jansampark/vashisthg/location/LocationUtils.java
+++ JanSampark/src/com/jansampark/vashisthg/location/LocationUtils.java
@@ -85,4 +85,61 @@ public final class LocationUtils {
         }
         return latLng;
     }
+    
+    
+    
+    
+    public static final int TEN_SECONDS = 10000;
+    public static final int TEN_METERS = 10;
+    public static final int TWO_MINUTES = 1000 * 60 * 2;
+
+ 	
+ 	 public static  Location getBetterLocation(Location newLocation, Location currentBestLocation) {
+ 	      if (currentBestLocation == null) {
+ 	          // A new location is always better than no location
+ 	          return newLocation;
+ 	      }
+
+ 	      // Check whether the new location fix is newer or older
+ 	      long timeDelta = newLocation.getTime() - currentBestLocation.getTime();
+ 	      boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+ 	      boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+ 	      boolean isNewer = timeDelta > 0;
+
+ 	      // If it's been more than two minutes since the current location, use the new location
+ 	      // because the user has likely moved.
+ 	      if (isSignificantlyNewer) {
+ 	          return newLocation;
+ 	      // If the new location is more than two minutes older, it must be worse
+ 	      } else if (isSignificantlyOlder) {
+ 	          return currentBestLocation;
+ 	      }
+
+ 	      // Check whether the new location fix is more or less accurate
+ 	      int accuracyDelta = (int) (newLocation.getAccuracy() - currentBestLocation.getAccuracy());
+ 	      boolean isLessAccurate = accuracyDelta > 0;
+ 	      boolean isMoreAccurate = accuracyDelta < 0;
+ 	      boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+ 	      // Check if the old and new location are from the same provider
+ 	      boolean isFromSameProvider = isSameProvider(newLocation.getProvider(),
+ 	              currentBestLocation.getProvider());
+
+ 	      // Determine location quality using a combination of timeliness and accuracy
+ 	      if (isMoreAccurate) {
+ 	          return newLocation;
+ 	      } else if (isNewer && !isLessAccurate) {
+ 	          return newLocation;
+ 	      } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+ 	          return newLocation;
+ 	      }
+ 	      return currentBestLocation;
+ 	  }
+ 	   /** Checks whether two providers are the same */
+ 	   private static boolean isSameProvider(String provider1, String provider2) {
+ 	       if (provider1 == null) {
+ 	         return provider2 == null;
+ 	       }
+ 	       return provider1.equals(provider2);
+ 	   }
 }
