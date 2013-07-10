53d23acabeedbeab37dab053692e457f7eb61cc8
gvv@fueled.co <gvv@fueled.co>
2013-07-10 18:42:36 +0530

    adding issue detail activity

--- JanSampark/src/com/jansampark/vashisthg/IssueDetailsActivity.java
+++ JanSampark/src/com/jansampark/vashisthg/IssueDetailsActivity.java
@@ -2,11 +2,16 @@ package com.jansampark.vashisthg;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 public class IssueDetailsActivity extends FragmentActivity {
 
 	public static final String EXTRA_ISSUE_ITEM = "issueItem";
 	private IssueItem issueItem;
+	
+	private TextView categoryTV;
+	private TextView nameTV;
+	private TextView systemTV;
 	
 	
 	@Override
@@ -19,5 +24,17 @@ public class IssueDetailsActivity extends FragmentActivity {
 		} else {
 			issueItem = (IssueItem) savedInstanceState.getSerializable(EXTRA_ISSUE_ITEM);
 		}
+		
+		setViews();
+	}
+	
+	private void setViews() {
+		categoryTV = (TextView) findViewById(R.id.issue_detail_category_text);
+		nameTV = (TextView) findViewById(R.id.issue_detail_name_text);
+		systemTV = (TextView) findViewById(R.id.issue_detail_system_text);
+		
+		categoryTV.setText(issueItem.getIssueId() + "");
+		nameTV.setText(issueItem.getIssueName());
+		systemTV.setText(IssueFactory.getIssueTypeString(this, issueItem.getTemplateId()));
 	}
 }
