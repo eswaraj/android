b18313016396e14b64d80fd5213795e924ac4349
vikas <v4kashyap@gmail.com>
2013-07-16 18:31:32 +0530

    Autocomplete text view and adapter added

--- JanSampark/src/com/jansampark/vashisthg/MainAnalyticsFragment.java
+++ JanSampark/src/com/jansampark/vashisthg/MainAnalyticsFragment.java
@@ -1,19 +1,35 @@
 package com.jansampark.vashisthg;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.achartengine.GraphicalView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AutoCompleteTextView;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.jansampark.vashisthg.adapters.LocationAutoCompleteAdapter;
+import com.jansampark.vashisthg.models.Location;
 import com.jansampark.vashisthg.widget.PieChartView;
 
 public class MainAnalyticsFragment extends Fragment {
@@ -21,14 +37,53 @@ public class MainAnalyticsFragment extends Fragment {
 	TextView issueNumTV;
 	TextView complaintsNumTV;
 	CompoundButton overallButton;
+	CompoundButton autoCompleteButton;
 	MyCount issueCounter;
 	MyCount complaintCounter;
+	
+	private AutoCompleteTextView autoCompleteTextView;
+	private View overlay;
+	private List<Location> locations;
+	private Location lastSelectedLocation;
 	//Spinner spinner;
 
 	int[] vals;
 
 	public static MainAnalyticsFragment newInstance(Bundle args) {
 		return new MainAnalyticsFragment();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+	private void parseLocations() {
+		try {
+			readLocations();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void readLocations() throws IOException {
+		InputStream inputStream = getActivity().getAssets().open("centroid.txt");
+		BufferedReader f = new BufferedReader(new InputStreamReader(inputStream));
+		String line;
+		locations = new ArrayList<Location>();
+		while ((line = f.readLine()) != null) {
+			String row[] = line.split(",");
+			parseRow(row);
+		}
+	}
+
+	private void parseRow(String[] row) {
+		Location loc = new Location();
+		loc.setName(row[0]);
+		loc.setID(Integer.parseInt(row[1].trim()));
+		loc.setLatLong(new LatLng(Double.parseDouble(row[2]), Double.parseDouble(row[3])));
+		locations.add(loc);
 	}
 
 	@Override
@@ -40,12 +95,13 @@ public class MainAnalyticsFragment extends Fragment {
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		setViews();
+		setViews(view);
 	}
 
-	private void setViews() {
+	private void setViews(View view) {
 		setButtons();
 		setPieChart();
+		setAutoComplete(view);
 		//setLocationSpinner();
 	}
 
@@ -132,6 +188,66 @@ public class MainAnalyticsFragment extends Fragment {
 	private void setButtons() {
 		overallButton = (CompoundButton) getActivity().findViewById(
 				R.id.analytics_overall);
+		autoCompleteButton = (CompoundButton) getActivity().findViewById(R.id.analytics_spinner);
+		autoCompleteButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				onRadioClick();
+			}
+
+		});
+	}
+	
+	private void onRadioClick() {
+		if(autoCompleteTextView.getVisibility() == View.VISIBLE) {
+			disableAutoComplete();
+		} else {
+			if(null != lastSelectedLocation) {
+				autoCompleteTextView.setText(lastSelectedLocation.getName());
+			}
+			autoCompleteTextView.setVisibility(View.VISIBLE);
+			overlay.setVisibility(View.VISIBLE);
+			autoCompleteTextView.requestFocus();
+			InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+		}
+		
+	}
+
+	private void disableAutoComplete() {
+		autoCompleteTextView.setVisibility(View.GONE);
+		overlay.setVisibility(View.INVISIBLE);
+	}
+	
+	private void setAutoComplete(View view) {
+		parseLocations();
+		autoCompleteTextView = (AutoCompleteTextView) view.findViewById(R.id.analytics_autocomplete);
+		overlay = view.findViewById(R.id.analytics_overlay);
+		overlay.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				disableAutoComplete();
+			}
+		});
+		LocationAutoCompleteAdapter adapter = new LocationAutoCompleteAdapter(getActivity(), locations);
+		autoCompleteTextView.setAdapter(adapter);
+		autoCompleteTextView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				disableAutoComplete();
+				Location loc = (Location) parent.getAdapter().getItem(position);
+				setLocation(loc);
+			}
+		});
+		
+	}
+
+	private void setLocation(Location loc) {
+		lastSelectedLocation = loc;
+		autoCompleteButton.setText(loc.getName());
 	}
 
 //	private void setLocationSpinner() {
